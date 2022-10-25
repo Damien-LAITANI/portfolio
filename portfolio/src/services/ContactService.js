45664f1c90f478d1650a9f56f25eb4fedb6a8739
Damien Laitani <d.laitani@gmail.com>
2022-10-25 14:27:05 +0200

    utilisation d'une variable d'environnement pour l'url du backend dans le service ContactService.js

--- portfolio/src/services/ContactService.js
+++ portfolio/src/services/ContactService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://portfolio.local',
+    baseURL: process.env.VUE_APP_API_URL,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
